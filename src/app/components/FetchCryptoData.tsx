59616810b21f9385e082be5d0d127a4f95d1df9a
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-03-10 15:35:19 -0400

    fixed type defniton and added ternary

--- src/app/components/FetchCryptoData.tsx
+++ src/app/components/FetchCryptoData.tsx
@@ -5,18 +5,18 @@ import UseCoinGeckoApi from './UseCoinGeckoApi';
 
 
 const FetchCryptoData = () => {
-    
     const { data: state, error: error } = UseCoinGeckoApi();
 
     const [search, setSearch] = React.useState("");
 
-    const handleChange = (e) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
     }
 
-    const filteredCoins = state && state.filter((coin) =>
-        coin.name.toLowerCase().includes(search.toLowerCase())
-    );
+    const filteredCoins = !!state
+        ? state.filter((coin) =>
+        coin.name.toLowerCase().includes(search.toLowerCase())) 
+        : []
 
     if (error) {
         return <div>Error: {error} </div>;
@@ -27,7 +27,7 @@ const FetchCryptoData = () => {
                 <input type="text" onChange={handleChange} />
             </div>
             <div>
-                {filteredCoins && filteredCoins.map((coin) => {
+                {filteredCoins.map((coin) => {
                     return (
                     <div
                     style={{
