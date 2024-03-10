33e1718b287b0a9fed8901e0f2bf080ff690497f
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2024-03-10 16:50:10 -0400

    Style the Etire app

--- src/app/components/CryptoCard.tsx
+++ src/app/components/CryptoCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-
+import "../styles/style.css";
 
 interface CryptoCardProps {
   image: string,
@@ -11,16 +11,7 @@ interface CryptoCardProps {
 
 const CryptoCard = ( props: CryptoCardProps) => {
     return (
-      <div
-        style={{
-                border: "1px solid #ccc",
-                borderRadius: "5px",
-                padding: "20px",
-                margin: "5px",
-                width: "30vw",
-                textAlign: "center",
-        }}
-      >
+      <div className="crypto-card">
         <img src={props.image} alt="crypto" />
         <h1>{props.name}</h1>
         <p>${props.price}</p>
